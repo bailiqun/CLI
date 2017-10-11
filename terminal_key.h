c1f0fdd21668239d431b6b8f2c79ea4f051b18fa
bailiqun <bailiqun@gmail.com>
2017-10-11 20:05:33 +0800

    change name

---
+++ terminal_key.h
@@ -0,0 +1,21 @@
+#ifndef ASCIII_KEY_H
+#define ASCIII_KEY_H
+
+#define KEY_ASCIII_ESCAPE  (27)
+
+#define KEY_ASCIII_RETURN  '\r'
+#define KEY_ASCIII_ENTER   '\n'
+#define KEY_ASCIII_TAB     '\t'
+#define KEY_ASCIII_SHIFT   (16)
+
+#define KEY_ASCIII_UP      (38)
+#define KEY_ASCIII_DOWN    (40)
+#define KEY_ASCIII_LEFT    (37)
+#define KEY_ASCIII_RIGHT   (39)
+
+#define KEY_ASCIII_BACK    (0x08)
+#define KEY_ASCIII_DELETE  (0x7F)
+
+#define KEY_ASCIII_BREAK   (0x03)
+
+#endif // ASCIII_KEY_H
