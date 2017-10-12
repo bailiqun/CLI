98e7e18091681fce0140533731d5a91152ea65b0
JoneLee <Jones_Lee3@163.com>
2017-10-12 18:18:02 +0800

    c mini terminal. it can be used in the usart IRQ.

--- cmd_help.h
+++
@@ -1,6 +0,0 @@
-#ifndef CMD_HELP_H
-#define CMD_HELP_H
-
-void HelpCmdExeFun(int agrc, char *argv[]);
-
-#endif // CMD_HELP_H
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-    terminal_spin();
-    return 0;
-}
